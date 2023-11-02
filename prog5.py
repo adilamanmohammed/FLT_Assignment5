bfe7d36513edf643e5bb93d9ce303f875de5f8d5
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-11-02 06:35:56 -0500

    Update prog5.py

--- prog5.py
+++ prog5.py
@@ -1,3 +1,11 @@
+"""
+Name : Adil Aman Mohammed
+Course : Formal language theory
+Assignment no: 5
+CWID : A20395630
+Description: the below code is an implementation of taking input CFG and applying 3 algorithms (Remove immediate left recursion, Remove left recursion, Remove unit productions)
+"""
+
 import sys
 
 def readGrammarSymbols(filename):
